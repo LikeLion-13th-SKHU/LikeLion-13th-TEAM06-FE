e938aae4ed3a0bf81f735e470ba0b99c82e0ecbf
gracekim527 <k07173027@gmail.com>
2025-08-12 23:22:06 +0900

    :package: Chore: 패키지 추가 설치 svg

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
   css: {
     preprocessorOptions: {
       scss: {
