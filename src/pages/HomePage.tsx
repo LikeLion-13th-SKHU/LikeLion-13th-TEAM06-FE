15c7fb29075e40ac0082e828909c6208d576ff5c
gracekim527 <k07173027@gmail.com>
2025-08-25 21:14:09 +0900

    :sparkles: Feat: 메인 페이지 내 맞춤형 소식 닉네임 변경

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@
 import { useHeader } from '@/shared/contexts/HeaderContext';
 import { useEffect } from 'react';
 import NewsSection from '@/features/news/components/NewsSection/NewsSection';
-
+import { useMyPageInfo } from '@/features/mypage/hooks/useMyPage';
 export default function HomePage() {
   const { setConfig } = useHeader();
 
@@ -18,10 +18,17 @@ export default function HomePage() {
     });
   }, []);
 
+  const { data: user } = useMyPageInfo();
+  if (!user) return <div>User not found</div>;
+
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: '32px', margin: '16px 0' }}>
       <NewsSection title="🔥 실시간 HOT ISSUE" link="/news/hot" type="hot" />
-      <NewsSection title="💡 은혜님 맞춤형 소식" link="/news/for-you" type="for-you" />
+      <NewsSection
+        title={`💡 ${user.nickname}님 맞춤형 소식`}
+        link="/news/for-you"
+        type="for-you"
+      />
       <NewsSection title="☘️ 우리 동네 소식" link="/news/local" layout="column" type="local" />
       <NewsSection title="🎨 행사/전시" link="/events" layout="events" type="events" />
     </div>
